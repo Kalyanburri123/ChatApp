7ffb66d6eaf1dd818b2b972842b94d1634637095
DELL <kalyanburri874@gmail.com>
2024-06-13 23:52:32 +0530

    first commit

---
+++ chatappproject/src/main/java/chatappproject/chatappproject/ChatappprojectApplication.java
@@ -0,0 +1,13 @@
+package chatappproject.chatappproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatappprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatappprojectApplication.class, args);
+	}
+
+}
---
+++ chatappproject/src/test/java/chatappproject/chatappproject/ChatappprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package chatappproject.chatappproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatappprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
